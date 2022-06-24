2566d5ed2b6a0851a2b8b8bcb4a59753b864bec2
Fedor Sharov <1710193@mail.ru>
2022-06-24 07:12:38 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int wallet = 100; // счет клиента 100 рублей;
+        int payment = 1100; // сумма пополнения счета;
+
+        int bonus;
+        if (payment > 1000) {
+            bonus = payment / 100;
+        } else {
+            bonus = 0;
+        }
+        int total = wallet + payment + bonus;
+
+        System.out.println(" У Вас на счету " + total + " рублей(я)");
+
+
+    }
+}
